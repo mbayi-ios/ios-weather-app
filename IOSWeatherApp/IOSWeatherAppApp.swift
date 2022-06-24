7102480aa39f288c37ef1a4da0572ce2f331e1c0
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-06-24 14:05:56 +0300

    Initial Commit

---
+++ IOSWeatherApp/IOSWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSWeatherAppApp.swift
+//  IOSWeatherApp
+//
+//  Created by Amby on 24/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct IOSWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
